2b536d894e912f6233a7a3fc8939b4414797bfe7
Yumat3 <yumat3@nycstudents.net>
2025-12-04 16:55:40 +0000

    st

---
+++ VR_Unit2/VRPractical2_5/car.js
@@ -0,0 +1,11 @@
+class Car{
+    constructor(x,y,z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.obj = car.cloneNode(true);
+        this.obj.setAttribute("position",{x:x, y:y, z:z});
+        scene.append(this.obj);
+    }
+    
+}
--- VR_Unit2/VRPractical2_5/script.js
+++ VR_Unit2/VRPractical2_5/script.js
@@ -9,20 +9,20 @@
    position them in the maze.   In Challenge 3 and 4, you will generate the maze along with any other object 
    you chose to put in the maze.  Get Creative!
 */
-
-let maze = [
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
+let ufos = [], maze = [
+  "xx-xxxxxxxxx--xxxxxxx",
+  "--x-x--t--------------",
+  "xx-xx-x--xxxxxxxx--xxx",
+  "x-------xxoxxo-----xxx",
+  "xx-x-xxxxx-xxxxxxxxxxx",
+  "xx-x-x-x-------------t",
+  "x-tx-o-x-xxoxxx-xxxxxx",
+  "x-xx-x-x-x--------txox",
+  "x------x-t-x-x--xxxx-x",
+  "xx-xtx---x---xx------x",
+  "-t---xttxx-xx---xxx--x",
+  "xxxxxoxxoxxtxxxxxxx--x",
 ];
 
 /* Challenge 2
@@ -33,14 +33,34 @@ let scene;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
+  car = document.getElementById("cyber");
   for(let r = 0; r < maze.length; r++){
     /* Challenge 3
       Choose a technique to traverse the each character in the string.
     */ 
     /* Challenge 4
-       Make an appropriate decision based on the characters you chose to enter 
+       Make an appropriate decision based on the characterdss you chose to enter 
        in the maze.  Create an instance of the corresponding object.
     */
+    let row = maze[r];
+    let cols = row.split("");
+    for(let c = 0; c < cols.length; c++){
+      if(cols[c] == "x"){
+        new Car(c*6,1,r*2);
+      }
+      else if(cols[c] == "t"){
+        let ufo = new UFO(c*6,10,r*2);
+        ufos.push(ufo);
+      }else if(cols[c]=="o"){
+        new Rock(c*6,1,r*2);
+      }
+    }
   }
-
+  loop();
 })
+function loop(){
+  for(let ufo of ufos){
+    ufo.invade();
+  }
+  window.requestAnimationFrame( loop );
+}
